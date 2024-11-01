103162b6d292102e947f1f71ac79aad036ae2202
LPT-24 <LPT-24@10.0.0.118>
2024-11-01 15:53:36 +0530

    first commit

---
+++ SVKP_college/src/basicsofjava/DoWhile.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("Welcome to c2tc session");
+			i++
+			while(i<=10);
+		}
+
+	}
+
+}
---
+++ SVKP_college/src/basicsofjava/DoWhileDemo.java
@@ -0,0 +1,18 @@
+package basicsofjava;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("welcome to c2tc session");
+			i++;
+		}while(i<=10);
+		
+	}
+}
+		
+
+	
+
+
