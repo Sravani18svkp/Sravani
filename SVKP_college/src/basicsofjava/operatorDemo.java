4e537ecb2c6df8c03ade1e9382f25d88c9a5be1f
LPT-24 <LPT-24@192.168.1.38>
2024-11-18 13:59:31 +0530

    3rd commit

---
+++ SVKP_college/src/basicsofjava/operatorDemo.java
@@ -0,0 +1,26 @@
+package basicsofjava;
+
+public class operatorDemo {
+	//Arithematic
+		public static void main(String args[]) {
+			int a=10,b=5;
+			//addition
+			int sum=a+b;
+			System.out.println("Addition:"+sum);
+			//subtraction
+			int different=a-b;
+			System.out.println("subtraction:"+different);
+			//multiplication
+			int product=a*b;
+			System.out.println("multiplication:"+product);
+			//division
+			int quotient=a/b;
+			System.out.println("Division:"+quotient);
+			//modulus
+			int remainder=a%b;
+			System.out.println("modules:"+remainder);
+			}
+
+	}
+
+
