e97fa507d480e3fa4c8025a0ae81327405c05c0d
LPT-24 <LPT-24@DESKTOP-17BIJRJ>
2024-11-19 14:18:32 +0530

    4th commit

---
+++ SVKP_college/src/basicsofjava/RelationalDemo.java
@@ -0,0 +1,19 @@
+package basicsofjava;
+
+public class RelationalDemo {
+
+	public static void main(String[] args) {
+int a=10 ,b=5;
+		
+		System.out.println("a==b "+ (a==b)); //equal to //false
+		System.out.println("a!=b " + (a!=b)); //Not equal to //true
+		System.out.println("a>b " + (a>b)); //Greater than //true
+		System.out.println("a<b " + (a<b)); //less than //false
+		System.out.println("a>=b " + (a>=b));//greater than or equal to //true
+		System.out.println("a<=b " + (a<=b));//less than or equal to //false
+
+	
+
+	}
+
+}
