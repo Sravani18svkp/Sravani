329dd5c7ee30292843fe0318946c2b118af22913
LPT-24 <LPT-24@192.168.1.78>
2024-12-19 14:14:10 +0530

    5th commit

---
+++ SVKP_college/src/scannerclass_bufferreader/ScannerClassDemo.java
@@ -0,0 +1,30 @@
+package scannerclass_bufferreader;
+
+public class ScannerClassDemo {
+
+//Import the Scanner class
+//Create a scanner object
+//prompt the user for input
+//Read input using scanner method
+//Validate
+//Close the scanner
+
+public class ScannerClassDemo {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter your name");
+		String name=scan.nextLine();
+		System.out.println("enter your age");
+		int age=scan.nextInt();
+	
+		System.out.println("name :"+ name +"  " +" age : "+ age);
+		scan.close();
+	}
+}
+}
+		
+		
+	
+
+
